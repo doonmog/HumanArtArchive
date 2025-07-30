3e8e7db9f8401e8a9af51e3052aec48464419afb
moondog-matthew <94130507+doonmog@users.noreply.github.com>
2025-07-30 18:38:09 +0200

    Cleanup and unify api endpoint

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui']
+  modules: ['@nuxt/image', '@nuxt/ui'],
+  routeRules: {
+    '/api/**': { proxy: 'http://back:3001/**' }
+  }
 })
