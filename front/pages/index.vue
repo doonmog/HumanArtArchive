cdc2bc4556686b0b60345003468af16520671559
moondog-matthew <94130507+doonmog@users.noreply.github.com>
2025-08-01 01:49:01 +0200

    Move test page away
    Now the real work can begin

---
+++ front/pages/index.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import { ref } from 'vue'
+
+const title = ref('Human Art Archive')
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-6xl font-bold">Human Art Archive</h1>
+    <NuxtLink to="/test" class="mt-8 text-xl text-blue-500 hover:underline">Go to Test Page</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
