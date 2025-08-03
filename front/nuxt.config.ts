5569c7a0a942b38b80018f7b85132ffb0140587a
moondog-matthew <94130507+doonmog@users.noreply.github.com>
2025-08-03 04:23:36 +0200

    Test optimization

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
   routeRules: {
     '/api/**': { proxy: 'http://back:3001/**' }
   }
